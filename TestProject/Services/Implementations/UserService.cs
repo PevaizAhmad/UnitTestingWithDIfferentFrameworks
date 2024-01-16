6a728af55109c6fe0be63930d076427ea0f73939
Pervaiz Ahmad <pervaiz.ahmad@speridian.com>
2024-01-16 16:35:52 +0500

    1 - Created two new projects for MSUnitTest and xUnit frameworks 2 - Created a testproject to implement the tests scenarios 3 - Write some test cases that executed as expected 4 - Used FluentAssertion Library

---
+++ TestProject/Services/Implementations/UserService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Services.Implementations
+{
+    public class UserService
+    {
+        public string getName()
+        {
+            return "Pervaiz Ahmad";
+        }
+
+        public string getCombinedNameAndEmail(string name, string email)
+        {
+            return $"{name} <{email}>";
+        }
+
+        public bool isEmailConfirmed()
+        {
+            return true;
+        }
+
+        public DateTime getUserCreatedDate()
+        {
+            return DateTime.Now;
+        }
+
+        public UserModel getUser()
+        {
+            return new UserModel
+            {
+                Id = 1,
+                Name = getName(),
+                Email = "pervaiz.ahmad@speridian.com"
+            };
+        }
+
+        public IEnumerable<UserModel> getUsers()
+        {
+            IEnumerable<UserModel> users = new List<UserModel>()
+            {
+                new UserModel
+                {
+                    Id = 1,
+                    Name = "Pervaiz Ahmad",
+                    Email = "pervaiz.ahmad@speridian.com",
+                    IsDeleted = false,
+                },
+                new UserModel
+                {
+                    Id = 2,
+                    Name = "Umer Awais Malik",
+                    Email = "umer.awais@speridian.com",
+                    IsDeleted = false,
+                },
+                new UserModel
+                {
+                    Id = 3,
+                    Name = getName(),
+                    Email = "ashraf.faheem@speridian.com",
+                    IsDeleted = false,
+                }
+            };
+
+            return users;
+        }
+    }
+}
---
+++ xUnit.Test/Services/UserServiceTest.cs
@@ -0,0 +1,102 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject;
+using TestProject.Services.Implementations;
+
+namespace xUnit.Test.Services
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void UserService_GetName_ReturnString()
+        {
+            //Arrange - variables, classes, mocks
+            var userService = new UserService();
+            //Act
+            var result = userService.getName();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Pervaiz Ahmad");
+        }
+
+        [Theory]
+        [InlineData("Pervaiz Ahmad", "pervaiz.ahmad@speridian.com", "Pervaiz Ahmad <pervaiz.ahmad@speridian.com>")]
+        public void UserService_GetCombinedNameAndEmail_ReturnString(string name, string email, string expectedResult)
+        {
+            //Arange
+            var userService = new UserService();
+
+            //Act
+            var result = userService.getCombinedNameAndEmail(name, email);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void UserService_IsEmailConfirmed_ReturnBoolean()
+        {
+            //Arrange
+            var userService = new UserService();
+
+            //Act
+            var result = userService.isEmailConfirmed();
+
+            //Assert
+            result.Should().BeTrue();
+            result.Should().NotBe(false);
+        }
+
+        [Fact]
+        public void UserService_GetUserCreatedDate_ReturnDate()
+        {
+            //Arrange - variables, classes, mocks
+            var userService = new UserService();
+            //Act
+            var result = userService.getUserCreatedDate();
+
+            //Assert
+            result.Should().NotBeBefore(1.January(2024));
+            result.Should().NotBeAfter(17.January(2024));
+        }
+
+        [Fact]
+        public  void UserService_GetUser_ReturnObject()
+        {
+            var userService = new UserService();
+            var result = userService.getUser();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<UserModel>();
+
+            //we can compare a property of an object
+            result.Name.Should().Be("Pervaiz Ahmad");
+
+        }
+        [Fact]
+        public  void UserService_GetUsers_ReturnObject()
+        {
+            var userService = new UserService();
+            var result = userService.getUsers();
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<IEnumerable<UserModel>>();
+
+            result.Should().HaveCountGreaterThanOrEqualTo(1);
+            //result.Should().HaveCount(1);  // if we want exact number of count
+            result.Should().Contain(x=>x.IsDeleted == false);
+
+        }
+    }
+}
